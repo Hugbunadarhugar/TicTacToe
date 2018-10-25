62134e0feb0c60cb8f151555f9ee2547d5218eb2
nannalop <natalia17@ru.is>
2018-10-25 14:00:25 +0000

    Everyone aboard to the temp branch

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
-const _= require("lodash");
+const _ = require("lodash");
 
 function component() {
     let element = document.createElement('div');  
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+module.exports = {
+
+    plugins: [    
+        new HtmlWebpackPlugin({
+            template: "./src/client/index.html",
+            title: 'TicTacToe page'
+        })],
+
+    entry: './src/client/index.js',
+    output: {
+        filename: './dist/index.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
